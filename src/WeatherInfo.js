e6c28a07134917c65f4519ff291a76de6ef8d0a1
“Korby <jacksonkorbyr@johndeere.com>
2020-08-21 15:30:09 -0500

    Added WeatherInfo files to app

---
+++ src/WeatherInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WeatherInfo extends Component {
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }
+  }
+
+
+export default WeatherInfo;
