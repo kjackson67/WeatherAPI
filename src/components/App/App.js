6c4bf188ade6fb3c258feb80212abc9be4fb6420
Brian Lerwick <lerwickbrianw@johndeere.com>
2020-08-21 17:33:10 -0500

    add router function to app.js and index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import WeatherInfo from "../WeatherInfo/WeatherInfo";
 import ForecastDetail from "../ForecastDetail/ForecastDetail";
 import Nav from "../Nav/Nav";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Nav />
-        <WeatherInfo />
-        <ForecastDetail />
-      </header>
-    </div>
-  );
+import { Route, Switch, Link, Redirect } from "react-router-dom";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+        </header>
+        <main>
+          <Switch>
+            <Link
+              to="/weatherinfo"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Weather Details
+            </Link>
+
+            <Route
+              path="/weatherinfo"
+              render={(routerProps) => (
+                <WeatherInfo {...this.state} {...routerProps} />
+              )}
+            />
+          </Switch>
+        </main>
+        {/* <ForecastDetail /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
