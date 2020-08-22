b08875f4dcfa9bcc29cd7a63bb2323a2957c9616
Brian Lerwick <lerwickbrianw@johndeere.com>
2020-08-22 11:41:38 -0500

    added code for ip lat/lon extraction and get weather by IP

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 import WeatherInfo from "../WeatherInfo/WeatherInfo";
 import ForecastDetail from "../ForecastDetail/ForecastDetail";
 import Nav from "../Nav/Nav";
 import { Route, Switch, Link, Redirect } from "react-router-dom";
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      lat: "",
+      lon: "",
+      ipWeather: [],
+    };
   }
+
+  componentDidMount = async () => {
+    let response = await navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // let lat = position.coords.latitude;
+        // let lon = position.coords.longitude;
+        // console.log("this is position", position);
+        // console.log("this is your current LAT", lat);
+        // console.log("this is your current LONG", lon);
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        console.log(this.state.lat);
+        console.log(this.state.lon);
+        this.getWeatherByIP();
+      }
+    );
+  };
+
+  getWeatherByIP = async () => {
+    // event.preventDefault();
+    let response = await axios
+      .get(
+        `https://api.weatherbit.io/v2.0/current?key=367fe182e3524d8fa57fc76ede94121e&units=I&lat=41.8781136&lon=-87.6297982`
+      )
+      .then((response) => {
+        console.log(response.data.data);
+        this.setState({
+          ipWeather: response.data.data,
+        });
+        console.log(this.state.ipWeather[0].city_name);
+      });
+  };
   render() {
+    let weatherByIp = this.state.ipWeather.map((item, index) => {
+      // console.log(image.url);
+      return item;
+    });
+
+    // let weatherByIp = this.state.ipWeather[0];
+    console.log(weatherByIp);
     return (
       <div className="App">
         <header className="App-header">
-          <Nav />
+          <nav>
+            <Nav />
+          </nav>
         </header>
         <main>
+          <div>
+            Latitude: {this.state.lat}
+            Longitude: {this.state.lon}
+            local weather: {weatherByIp.city_name}
+          </div>
           <Switch>
-            <Link
-              to="/weatherinfo"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              Weather Details
-            </Link>
+            <Link to="/weatherinfo">Weather Details</Link>
 
             <Route
               path="/weatherinfo"
