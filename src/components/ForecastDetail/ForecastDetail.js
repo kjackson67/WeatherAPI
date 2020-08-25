e85374f3606e40e323d9d0600de591fb55d2366c
Brian Lerwick <lerwickbrianw@johndeere.com>
2020-08-25 09:17:20 -0500

    formating of forecast and homepage weather data

--- src/components/ForecastDetail/ForecastDetail.js
+++ src/components/ForecastDetail/ForecastDetail.js
@@ -1,38 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ForecastDetail.css";
 
-function ForecastDetail(props) {
-  const forecastDetails = props.ipForecast.map((item, index) => {
-    return (
-      <div className="forecastWrapper" key={index}>
-        <div className="dailyForecastContainer">
-          <div className="date">
-            <div>{item.valid_date}</div>
+class ForecastDetail extends Component {
+  render() {
+    const forecastDetails = this.props.ipForecast.map((item, index) => {
+      let today = item.valid_date;
+      const getDayName = (today) => {
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let day = new Date(today).getUTCDay();
+        return days[day];
+      };
+      const formatDate = (today) => {
+        let date = new Date(today).getUTCDate();
+        let month = new Date(today).getMonth() + 1;
+        let datevalue = ` ${month}/${date}`;
+        console.log("this is today", datevalue);
+        return datevalue;
+      };
+      return (
+        <div className="forecastWrapper" key={index}>
+          <div className="dailyForecastContainer">
+            <div className="date">
+              <div>{getDayName(today) + formatDate(today)}</div>
+            </div>
+            <span className="tempRange">
+              <span style={{ color: "red" }}>{Math.round(item.max_temp)}</span>
+              <span> | </span>
+              <span style={{ color: "blue" }}>{Math.round(item.min_temp)}</span>
+              <span> °F</span>
+            </span>
+            <div className="conditionsIcon">
+              <img
+                className="icon"
+                src={`./icons/${item.weather.icon}.png`}
+                alt="icon"
+              />
+              <div className="skyConditions">{item.weather.description}</div>
+            </div>
+            <div className="windInformation">
+              <div>Wind Speed: {Math.round(item.wind_spd)} mph </div>
+              <div>Wind Direction: {item.wind_cdir} </div>
+              <div>Wind Direction: {item.wind_dir}° </div>
+            </div>
+            <div className="pop">
+              <div>
+                {item.pop > 0 ? (
+                  <img
+                    className="popicon"
+                    src={`./icons/tear.png`}
+                    alt="rain"
+                  />
+                ) : (
+                  <img className="popicon" src={`./icons/blur.png`} alt="dry" />
+                )}
+              </div>
+              <div>{item.precip.toFixed(2)}"</div>
+            </div>
+            <div> {(item.pres / 33.864).toFixed(2)} in</div>
           </div>
-          <div className="tempRange">
-            {Math.round(item.max_temp)} °F / {Math.round(item.min_temp)} °F
-          </div>
-          <div className="conditionsIcon">
-            <img
-              className="icon"
-              src={`./icons/${item.weather.icon}.png`}
-              alt="icon"
-            />
-            <div className="skyConditions">{item.weather.description}</div>
-          </div>
-          <div className="windInformation">
-            <div>Wind Speed: {Math.round(item.wind_spd)} mph </div>
-            <div>Wind Direction: {item.wind_cdir} </div>
-            <div>Wind Direction: {item.wind_dir}° </div>
-          </div>
-          <div>Probablity of Precipitation: {item.pop}%</div>
-          <div>{item.precip.toFixed(2)}</div>
-          <div>Pressure: {(item.pres / 33.864).toFixed(2)} inHg</div>
         </div>
-      </div>
-    );
-  });
-  return <div className="forecastContainer">{forecastDetails}</div>;
+      );
+    });
+    return <div className="forecastContainer">{forecastDetails}</div>;
+  }
 }
 
 export default ForecastDetail;
