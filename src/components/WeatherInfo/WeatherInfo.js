bcdadde271aace22e9c0143b566361041eadb918
Brian Lerwick <lerwickbrianw@johndeere.com>
2020-08-25 12:41:14 -0500

    resolve missing code in app.js complete color temp function

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -3,36 +3,33 @@ import "./WeatherInfo.css";
 import ForecastDetail from "../ForecastDetail/ForecastDetail";
 
 function WeatherInfo(props) {
-// const weatherUpdate = props.ipWeather.map((item, index) => {
+  // const weatherUpdate = props.ipWeather.map((item, index) => {
   return (
-    <div className="currentConditionsWrapper" >
+    <div className="currentConditionsWrapper">
       <div className="currentConditionsContainer">
         <h1 className="currentConditions">Current Conditions</h1>
         <div>
           {props.ipWeather.map((item, index) => {
             return (
-             <div key={index}>
-               <h2 className="currentconditionsLocation">
-                  {item.city_name}, {item.state_code} 
+              <div key={index}>
+                <button onClick={() => props.history.goBack()}>Home</button>
+                <h2 className="currentconditionsLocation">
+                  {item.city_name}, {item.state_code}
                 </h2>
-            {/* <div className="currentForecastWrapper">
-              <div className="currentForecastContainer">
-                <img className="conditionsIcon" src={`./icons/${item.weather.icon}.png`}
-                alt="icon" />
-                <div>{item.weather.description}</div>
-            </div> */}
-              <div className="tempContainer">
-                <div className="currentTempWrapper">
-                  <div className="curentTemp">{Math.round(item.temp)}</div>
+                <div className="tempContainer">
+                  <div className="currentTempWrapper">
+                    <div className="curentTemp">{Math.round(item.temp)}</div>
                     <div className="currentTempUnits"> °F</div>
-            </div>
+                  </div>
                   <div className="feelsLike">
                     <span>Feels Like</span> {Math.round(item.app_temp)}
                   </div>
                 </div>
                 <div className="conditionsDataWrapper">
                   <div className="conditionsDataContainer">
-                    <img className="conditionsIcon" src={`./icons/${item.weather.icon}.png`}
+                    <img
+                      className="conditionsIcon"
+                      src={`./icons/${item.weather.icon}.png`}
                       alt="icon"
                     />
                     <div>{item.weather.description}</div>
@@ -41,31 +38,33 @@ function WeatherInfo(props) {
                       <div>Wind Direction: {item.wind_cdir} </div>
                       <div>Wind Direction: {item.wind_dir}° </div>
                     </div>
-                  <div className="additionalConditionsContainer">
-                    <div className="additionalConditionsWrapper">
-                    <div>Pressure: {(item.pres / 33.864).toFixed(2)} in</div>
-                      <div>Visibility: {item.vis} miles</div>
-                      <div>Clouds: {item.clouds} %</div>
-                      <div>Dew Point: {item.dewpt} °F</div>
-                      <div>Humidity: {item.rh} %</div>
-                      <div>Rainfall: {item.precip.toFixed(2)} %</div>
-                      <div>Snow Depth: {item.snow} inches</div>
-                  </div>
+                    <div className="additionalConditionsContainer">
+                      <div className="additionalConditionsWrapper">
+                        <div>
+                          Pressure: {(item.pres / 33.864).toFixed(2)} in
+                        </div>
+                        <div>Visibility: {item.vis} miles</div>
+                        <div>Clouds: {item.clouds} %</div>
+                        <div>Dew Point: {item.dewpt} °F</div>
+                        <div>Humidity: {item.rh} %</div>
+                        <div>Rainfall: {item.precip.toFixed(2)} %</div>
+                        <div>Snow Depth: {item.snow} inches</div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            // </div>
-  )})}
-          </div>
+            );
+          })}
         </div>
+      </div>
       <ForecastDetail
         ipForecast={props.ipForecast}
         getForecastByIp={props.getForecastByIp}
       />
-  </div>
+    </div>
   );
-// return <div className="weatherContainer">{weatherUpdate}</div>
+  // return <div className="weatherContainer">{weatherUpdate}</div>
 }
 
 export default WeatherInfo;
