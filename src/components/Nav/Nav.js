674f91339b3a99e851ca0340fbdd37ad43ba8e89
Scott Gilson <gilsonscott@johndeere.com>
2020-08-25 12:39:01 -0500

    updated for today

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,6 +9,7 @@ class Nav extends Component {
       state: "",
       country: "",
     };
+
   }
   
 handleSubmit = (event) =>{
@@ -23,14 +24,11 @@ handleSubmit = (event) =>{
         <input onChange={this.props.handleChange} value={this.props.city} type="text" name="city" placeholder="City..."/>
         <input onChange={this.props.handleChange} value={this.props.state} type="text" name="state" placeholder="State..."/>
         <input onChange={this.props.handleChange} value={this.props.country} type="text" name="country" placeholder="Country..."/>
-
         <button >Get Weather</button>
       </form>
       
     ) 
   }
-  
-
 };
 
 export default Nav;
